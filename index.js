00daa7279903d7cb61e373b3e8bc6e936544fafa
mustahsin2000 <mustahsin152415@gmail.com>
2022-11-10 17:22:37 +0600

    review

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -17,11 +17,37 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wfblndv.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+
+async function run(){
+try{
+const serviceCollect = client.db('dentalClient').collection('services');
+const reviewCollect = client.db('dentalClient').collection('review');
+app.get('/services', async(req,res)=>{
+    const query = {};
+    const cursor = serviceCollect.find(query);
+    const services = await cursor.toArray();
+    res.send(services);
 });
+
+app.get('/services/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id:ObjectId(id)};
+    const service = await serviceCollect.findOne(query);
+    res.send(service);
+});
+
+//review
+app.post('/review',async(req,res)=>{
+    const review = req.body;
+    const result = await reviewCollect.insertOne(review);
+    res.send(result);
+})
+
+}finally{
+
+}
+}
+run().catch(err=>console.error(err))
 
 
 app.get('/',(req,res)=>{
