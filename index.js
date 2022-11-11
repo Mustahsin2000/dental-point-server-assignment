53f4c9f6497d6c2b8930d6302dacbad3ab57265e
mustahsin2000 <mustahsin152415@gmail.com>
2022-11-11 15:22:52 +0600

    token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,6 +18,13 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wfblndv.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+app.post('/jwt',(req,res)=>{
+    const user = req.body;
+    console.log(user);
+    const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'10h'})
+    res.send({token})
+})
 
 async function run(){
 try{
